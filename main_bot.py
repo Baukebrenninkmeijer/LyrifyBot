687d82bd913909e4be215d6749b15bef48387508
baukebrenninkmeijer <bauke.brenninkmeijer@gmail.com>
2018-03-30 00:16:06 +0200

    Semi final bot push.

--- main_bot.py
+++ main_bot.py
@@ -3,10 +3,13 @@ import requests
 import time
 import urllib
 import re
+import woo
 
 
 TOKEN = open('config').read().rstrip()
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+data = woo.get_data()
+print "Data loaded"
 
 
 def get_url(url):
@@ -44,7 +47,7 @@ def get_last_chat_id_and_text(updates):
 
 
 def send_message(text, chat_id):
-    text = urllib.parse.quote_plus(text)
+    text = urllib.pathname2url(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
@@ -53,9 +56,34 @@ def echo_all(updates):
         try:
             text = update["message"]["text"]
             chat = update["message"]["chat"]["id"]
-            send_message(text, chat)
+            response = respond(text)
+            send_message(response, chat)
         except Exception as e:
             print(e)
+
+def respond(text):
+    artists_in_message = []
+    # print len(data.keys())
+    for artist in data.keys():
+        regex = r'\b(' + artist + r')\b|\A('+artist+r'\b)'
+        if re.search(regex, text, re.I):
+            artists_in_message.append(artist)
+    if len(artists_in_message) > 1:
+        print "triggered multiple artists: {}".format(artists_in_message)
+        regex_res = re.search(r'(\d+)', text, re.I)
+        if regex_res is not None:
+            return woo.get_mashup(artists_in_message, regex_res.group(0), data)
+        return woo.get_mashup(artists_in_message, data)
+    if len(artists_in_message) == 1:
+        print "triggered single artist: {}".format(artists_in_message)
+        regex_res = re.search(r'(\d+)', text, re.I)
+        if regex_res is not None:
+            return woo.get_lyric(artists_in_message[0], regex_res.group(0), data)
+        return woo.get_lyric(artists_in_message[0], data)
+    if re.search(r'\Ahey', text, re.I):
+        return "Hey! I'm the LyrifyBot and I can help you get lyrics inspired by specific artists."
+    if len(artists_in_message) == 0:
+        return "No artist found in message. Please send a new one!"
 
 
 def main():
@@ -69,4 +97,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print respond("hey botje")
---
+++ woo.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import markovify
+import re
+import gensim
+
+# data = ""
+
+def get_data():
+    df = pd.read_csv('songdata.csv')
+    pattern = re.compile('\n *\n')
+    data = {}
+    for index, row in df.iterrows():
+        artist = row['artist']
+        text = '\n'.join([t.strip().lower() for t in pattern.split(row['text'])])
+        if artist not in data:
+            data[artist] = text + '\n'
+        else:
+            data[artist] = data[artist] + text
+    return data
+
+class ArtistModel:
+    def __init__(self, artist, data, state_size=2):
+        self.model = markovify.NewlineText(data[artist], state_size=state_size)
+
+    def lyric(self, length=0):
+        length = int(length)
+        if length > 10000:
+            return "Too long length"
+        if length == 0:
+            return self.model.make_sentence()
+        if length > 0:
+            return self.model.make_short_sentence(length)
+
+    def finish_lyric(self, start):
+        try:
+            return self.model.make_sentence_with_start(start.strip().lower(), strict=False)
+        except:
+            return "Beginning not in corpus"
+
+    def mashup(self, models):
+        self.model = markovify.combine([self.model] + [model.model for model in models])
+
+
+def artists(data):
+    artists = list(data.keys())
+    artists.sort()
+    return artists
+
+
+def get_mashup(artists, length, data):
+    models = []
+    for artist in artists:
+        model = ArtistModel(artist, data)
+        models.append(model)
+    models[0].mashup(models[1:])
+    return models[0].lyric(length)
+
+
+def get_lyric(artist, length, data):
+    model = ArtistModel(artist, data)
+    return model.lyric(length)
+
+#
+# def main():
+#     get_data()
+#
+# if __name__ == '__main__':
+#     main()
+
+# data = get_data()
+# yeezy = ArtistModel('Kanye West', data)
+# linkin = ArtistModel('Linkin Park', data)
+# # print linkin.lyric()
+# print yeezy.mashup([linkin.model])
+# print yeezy.lyric()
+# model.finish_lyric('pain')
+# model.lyric()
